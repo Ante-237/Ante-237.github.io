45cdb68c8379eac47ff357d01577169366f24abe
anTe <a.nwalahnjie@alustudent.com>
2024-09-23 23:43:14 +0200

    update to porfolio

---
+++ responsive_design/sample.js
@@ -0,0 +1,24 @@
+let currentImageIndex = 0;
+const images = document.querySelectorAll('.image-carousel img');
+
+function showImage(index) {
+    images.forEach((img, i) => {
+        img.classList.remove('active');
+        if (i === index) {
+            img.classList.add('active');
+        }
+    });
+}
+
+function nextImage() {
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    showImage(currentImageIndex);
+}
+
+function prevImage() {
+    currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+    showImage(currentImageIndex);
+}
+
+// Initialize first image as active
+showImage(currentImageIndex);
